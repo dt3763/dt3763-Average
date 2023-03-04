84df033987a2fbea15e7e20cf5995f71247b8c2f
dt3763 <dt3763@gmail.com>
2023-03-04 09:33:13 +0000

    average

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -17,7 +17,15 @@ public class Average {
      * @param nums an array to be manipulated.
      * @return the average of all the values of nums.
      */
-    public double getAvg(int[] nums){
-        return 0;
+    public double getAvg(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException("Input array cannot be null or empty.");
+        }
+        double sum = 0.0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        return sum / nums.length;
     }
+    
 }
